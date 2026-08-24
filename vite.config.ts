1683b59cba49697c29c1b6238802602e704e4ec2
Dzmitry Buldyha <91372817+NotDmitry@users.noreply.github.com>
2026-08-24 22:43:08 +0300

    refactor: change vite.config to utilize import.meta.dirname

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import * as path from 'path';
+import path from 'node:path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,10 +8,10 @@ export default defineConfig({
   base: '/social-media-network/',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@widgets': path.resolve(__dirname, 'src/widgets'),
+      '@': path.resolve(import.meta.dirname, 'src'),
+      '@assets': path.resolve(import.meta.dirname, 'src/assets'),
+      '@pages': path.resolve(import.meta.dirname, 'src/pages'),
+      '@widgets': path.resolve(import.meta.dirname, 'src/widgets'),
     },
   },
 })
