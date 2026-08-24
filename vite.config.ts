40cd20472c19ad76619f41bda8641f0f3623c5ff
Dzmitry Buldyha <91372817+NotDmitry@users.noreply.github.com>
2026-08-24 18:36:11 +0300

    chore: add aliases for cleaner imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/social-media-network/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@widgets': path.resolve(__dirname, 'src/widgets'),
+    },
+  },
 })
