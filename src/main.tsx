d23444fad36a4e6fc0220deb205e23a69a33c096
Dzmitry Buldyha <91372817+NotDmitry@users.noreply.github.com>
2026-08-24 19:31:37 +0300

    feat: wrap App in BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
 import App from './app';
 
 const root = document.getElementById('root');
 if (root) {
   createRoot(root).render(
     <StrictMode>
-      <App />
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <App />
+      </BrowserRouter>
     </StrictMode>,
   );
 }
