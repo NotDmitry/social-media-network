fc6ee77cf53eaf5e435207b9eb88a9e95d3ee09d
Dzmitry Buldyha <91372817+NotDmitry@users.noreply.github.com>
2026-08-25 21:12:51 +0300

    feat: add application layout styles

--- src/app/Layout/index.tsx
+++ src/app/Layout/index.tsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router';
 import type { HeaderVariant } from '@widgets/Header/types';
 import Header from '@widgets/Header';
 import Footer from './Footer';
+import './style.css';
 
 interface LayoutProps {
   headerVariant: HeaderVariant;
@@ -9,13 +10,13 @@ interface LayoutProps {
 
 function Layout({ headerVariant }: LayoutProps) {
   return (
-    <>
+    <div className='layout-wrapper'>
       <Header variant={headerVariant}></Header>
-      <main>
+      <main className='layout-main'>
         <Outlet />
       </main>
       <Footer />
-    </>
+    </div>
   );
 }
 
