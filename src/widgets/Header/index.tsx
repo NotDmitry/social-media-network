902696b3a52cfa62f232c4097722de4d4e4521a4
Dzmitry Buldyha <91372817+NotDmitry@users.noreply.github.com>
2026-08-26 18:05:21 +0300

    feat: add Header with user authenticated variant

--- src/widgets/Header/index.tsx
+++ src/widgets/Header/index.tsx
@@ -1,13 +1,28 @@
 import type { HeaderVariant } from './types';
+import { ROUTES } from '@app/routes';
+import { Link } from 'react-router';
+import Logo from '@shared/ui/Logo';
+import './style.css';
+import userImage from '@assets/images/test_user_1.jpg';
 
 interface HeaderProps {
   variant: HeaderVariant;
 }
 
 function Header({ variant }: HeaderProps) {
-  // TODO: replace with actual component logic (console is for silencing the linter)
-  console.log(variant);
-  return <header></header>;
+  return (
+    <header className='header'>
+      <Logo />
+      {variant === 'guest' && <div></div>}
+
+      {variant === 'user' &&
+        <Link className='link' to={ROUTES.profile}>
+          <img className='avatar' src={userImage} width={24} height={24} />
+          Name Surname
+        </Link>
+      }
+    </header>
+  );
 }
 
 export default Header;
