6d043bf79055a79954597b95f9fdf53615e134ca
Dzmitry Buldyha <91372817+NotDmitry@users.noreply.github.com>
2026-09-02 17:13:02 +0300

    feat: add SignUp page layout

--- src/pages/SignUpPage/index.tsx
+++ src/pages/SignUpPage/index.tsx
@@ -1,5 +1,43 @@
+import { Link } from 'react-router';
+import SignUpForm from '@features/SignUpForm';
+import { ROUTES } from '@app/routes';
+
 function SignUpPage() {
-  return <div>Sign Up Page</div>;
+  return (
+    <div className='auth-page-container'>
+      <div className='auth-page-title-wrapper'>
+        <h1 className='auth-page-title'>Create an account</h1>
+        <p className='auth-page-text'>
+          Enter your profile credentials
+          <span>to sign up for this app</span>
+        </p>
+      </div>
+      <SignUpForm />
+      <p className='auth-page-agreement-text'>
+        By clicking continue, you agree to our{' '}
+        <a
+          className='auth-page-external-link'
+          href="https://policies.google.com/terms"
+          target='_blank'
+          rel='noreferrer'
+        >
+          Terms of Service
+        </a>
+        {' '}and{' '}
+        <a
+          className='auth-page-external-link'
+          href="https://policies.google.com/privacy"
+          target='_blank'
+          rel='noreferrer'
+        >
+          Privacy Policy
+        </a>
+      </p>
+      <p className='auth-page-text'>
+        Already have an account? <Link className='auth-page-link' to={ROUTES.signIn}>Sign in</Link>
+      </p>
+    </div >
+  );
 }
 
 export default SignUpPage;
