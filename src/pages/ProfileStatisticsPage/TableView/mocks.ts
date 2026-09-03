fd91ecfca49790c64b84999c1eb69d7d70f59bd0
Dzmitry Buldyha <91372817+NotDmitry@users.noreply.github.com>
2026-09-04 01:33:47 +0300

    feat: add TableView component

---
+++ src/pages/ProfileStatisticsPage/TableView/mocks.ts
@@ -0,0 +1,43 @@
+import type { TableViewProps } from "./index";
+
+export const TABLE_DATA: TableViewProps = {
+  caption: 'Title',
+  columnHeaders: ['Col1', 'Col2', 'Col3'],
+  data: [
+    {
+      rowHeading: 'Row 1',
+      firstDataSlot: 123,
+      secondDataSlot: 456,
+    },
+    {
+      rowHeading: 'Row 2',
+      firstDataSlot: 123,
+      secondDataSlot: 456,
+    },
+    {
+      rowHeading: 'Row 3',
+      firstDataSlot: 123,
+      secondDataSlot: 456,
+    },
+    {
+      rowHeading: 'Row 4',
+      firstDataSlot: 123,
+      secondDataSlot: 456,
+    },
+    {
+      rowHeading: 'Row 5',
+      firstDataSlot: 123,
+      secondDataSlot: 456,
+    },
+    {
+      rowHeading: 'Row 6',
+      firstDataSlot: 123,
+      secondDataSlot: 456,
+    },
+    {
+      rowHeading: 'Row 7',
+      firstDataSlot: 123,
+      secondDataSlot: 456,
+    },
+  ]
+}
