5d5c58b487fcb0546c6cb386caf192bd539d3008
Dzmitry Buldyha <91372817+NotDmitry@users.noreply.github.com>
2026-09-03 23:09:55 +0300

    feat: add StatsCard component

---
+++ src/pages/ProfileStatisticsPage/StatsCard/index.tsx
@@ -0,0 +1,23 @@
+import './style.css';
+
+interface StatsCardProps {
+  title?: string;
+  data?: string;
+  trendText?: string;
+}
+
+function StatsCard({
+  title = 'title',
+  data = '99,999.90',
+  trendText = '+99% month over month'
+}: StatsCardProps) {
+  return (
+    <article className='stats-card-container'>
+      <h3 className='stats-card-title'>{title}</h3>
+      <p className='stats-card-data'>{data}</p>
+      <p className='stats-card-trend'>{trendText}</p>
+    </article>
+  );
+}
+
+export default StatsCard;
