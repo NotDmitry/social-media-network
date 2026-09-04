2e065da985e5444f5061fa7ac1eb43e1bf636467
Dzmitry Buldyha <91372817+NotDmitry@users.noreply.github.com>
2026-09-04 16:30:15 +0300

    feat: add auth methods to context

--- src/entities/auth/context.ts
+++ src/entities/auth/context.ts
@@ -1,9 +1,13 @@
 import { createContext } from 'react';
+import type { SignInPayload, SignUpPayload } from './types';
 import type { UserModel } from '@entities/User/types';
 
 interface AuthContextData {
   currentUser: UserModel | null;
   isUserAuthenticated: boolean;
+  signIn: (user: SignInPayload) => void;
+  signUp: (user: SignUpPayload) => void;
+  signOut: () => void;
 }
 
 export const AuthContext = createContext<AuthContextData | null>(null);
