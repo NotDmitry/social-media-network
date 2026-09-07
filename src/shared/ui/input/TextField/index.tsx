6ca6de39f347230e988895ca725f55664c533715
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2026-09-07 15:51:29 +0300

    feat(ui): add reusable input fields (#6)
    * feat: add form-related icons as separate components
    
    * feat: add default styles for app icons
    
    * feat: add configurable reusable TextareaField component
    
    * feat: add common styles for input fields
    
    * feat: add configurable reusable TextField component
    
    * refactor: adjust common input field styles to be utilized by TextareaField
    
    * feat: add configurable reusable PasswordField component
    
    * fix: add missing bottom padding to the textarea
    
    * feat: collapse long input labels with ellipsis
    
    * fix: remove native 'show password' button on MS Edge
    
    * feat: add aria-attributes to custom input fields
    
    * fix: add long textarea hint collapsing
    
    * fix: add type='button' to password toggle visibility button in order to prevent unintentional form submission
    
    * refactor: make 'text' behavior default for TextField component instead of 'email'
    
    * refactor: control show password visibility button from props
    
    * fix: move aria-label attribute from svg icon to the corresponding button

---
+++ src/shared/ui/input/TextField/index.tsx
@@ -0,0 +1,87 @@
+import { useId } from 'react';
+import type { ComponentProps, ReactNode } from 'react';
+import { CheckIcon, CrossIcon, InfoIcon, InfoTooltipIcon, PencilIcon } from '@shared/icons';
+
+type TextFieldStatus = 'default' | 'valid' | 'invalid';
+
+interface TextFieldProps extends Omit<ComponentProps<'input'>, 'type'> {
+  label?: string;
+  labelIcon?: ReactNode;
+  status?: TextFieldStatus;
+  errorMessage?: string;
+  type?: 'text' | 'email';
+  tooltipMessage?: string;
+}
+
+function TextField({
+  label = 'Text',
+  labelIcon = <PencilIcon />,
+  status = 'default',
+  errorMessage,
+  type = 'text',
+  tooltipMessage,
+  className = '',
+  disabled,
+  id,
+  placeholder = 'Enter some text...',
+  ...restProps
+}: TextFieldProps) {
+  const privateId = useId();
+  const textInputId = id ?? privateId;
+
+  const isInvalid = status === 'invalid';
+  const isValid = status === 'valid';
+
+  let statusIcon = null;
+  if (isInvalid) {
+    statusIcon = <CrossIcon className='input-field-status-icon input-field-status-icon_invalid' />
+  } else if (isValid) {
+    statusIcon = <CheckIcon className='input-field-status-icon input-field-status-icon_valid' />
+  }
+
+  const validationMessage = isInvalid && errorMessage ? errorMessage : '';
+
+  return (
+    <div className='input-field-wrapper'>
+      <div className='input-field-label-wrapper'>
+        <label
+          className={`input-field-label ${disabled ? `input-field-label_disabled` : ''}`}
+          htmlFor={textInputId}
+          title={label}
+        >
+          {labelIcon}
+          <span className='input-field-label-text'>{label}</span>
+        </label>
+        {!disabled && statusIcon}
+      </div>
+
+      <input
+        {...restProps}
+        type={type}
+        className={`input-field ${isInvalid ? 'input-field_invalid' : ''} ${className}`}
+        disabled={disabled}
+        id={textInputId}
+        placeholder={placeholder}
+      />
+
+      {!disabled && validationMessage &&
+        <div className={`input-field-validation-wrapper`}>
+          <div
+            className={`input-field-validation ${isInvalid ? 'input-field-validation_invalid' : ''}`}
+            title={validationMessage}
+          >
+            <InfoIcon />
+            <span className='input-field-validation-text'>{validationMessage}</span>
+          </div>
+          {tooltipMessage &&
+            <span title={tooltipMessage} className='input-field-tooltip-icon' aria-label='Tooltip icon'>
+              <InfoTooltipIcon />
+            </span>
+          }
+        </div>
+      }
+    </div >
+  );
+}
+
+export default TextField;
