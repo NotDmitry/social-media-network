0ee03a25c1997a6c7ebc40539f78c559ffc2e90b
Dzmitry Buldyha <91372817+NotDmitry@users.noreply.github.com>
2026-09-10 16:37:16 +0300

    refactor: update mocks with new UserModel data shape

--- src/shared/mocks/PostMocks.ts
+++ src/shared/mocks/PostMocks.ts
@@ -6,21 +6,21 @@ import postImage3 from '@/assets/images/test_post_3.jpg';
 export const MOCK_POSTS: PostModel[] = [
   {
     id: 'post-1',
-    authorId: 'user-1',
+    authorId: 1,
     imageUrl: postImage1,
     description: 'Body text for a post. Sometimes it is an observation, and sometimes it is seeking recommendations.',
     createdAt: '2026-08-31T10:00:00.000Z',
   },
   {
     id: 'post-2',
-    authorId: 'user-2',
+    authorId: 2,
     imageUrl: postImage2,
     description: 'Look at my new CAAAAR 😍!',
     createdAt: '2026-08-31T11:00:00.000Z',
   },
   {
     id: 'post-3',
-    authorId: 'user-3',
+    authorId: 3,
     imageUrl: postImage3,
     description: '"Los Pollos Hermanos" now opening new location in Minsk, Belarus. 🎉🎉🎉',
     createdAt: '2026-08-31T12:00:00.000Z',
