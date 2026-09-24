70091c4d19242436df7b25a980d7a1d445ba1e99
Dzmitry Buldyha <91372817+NotDmitry@users.noreply.github.com>
2026-09-25 01:35:53 +0300

    feat: add backend suggestions lists

--- src/pages/HomePage/CardsList/index.tsx
+++ src/pages/HomePage/CardsList/index.tsx
@@ -9,14 +9,40 @@ export interface CardData {
 
 interface CardsListProps {
   title: string;
-  cardsData: CardData[];
+  cardsData?: CardData[];
+  isDataFetchPending: boolean;
+  isDataFetchError: boolean;
+  errorMessage?: string;
+  emptyListMessage?: string;
 }
 
-function CardsList({ title, cardsData }: CardsListProps) {
+function CardsList({
+  title,
+  cardsData,
+  isDataFetchPending,
+  isDataFetchError,
+  errorMessage,
+  emptyListMessage,
+}: CardsListProps) {
+  let cardsListStatusMessage: string | null = null;
+
+  if (isDataFetchPending) {
+    cardsListStatusMessage = 'Loading...';
+  } else if (isDataFetchError && cardsData === undefined) {
+    cardsListStatusMessage = errorMessage ?? 'Unable to load data';
+  } else if (cardsData === undefined || cardsData.length === 0) {
+    cardsListStatusMessage = emptyListMessage ?? 'No data yet';
+  }
+
   return (
     <div className='cards-list-container'>
       <h3 className='cards-list-title'>{title}</h3>
-      {cardsData.map((cardData) => (
+
+      {cardsListStatusMessage &&
+        <p className='cards-list-message'>{cardsListStatusMessage}</p>
+      }
+
+      {cardsData?.map((cardData) => (
         <article className='cards-list-item-container' key={cardData.id}>
           <img
             className='avatar'
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,19 +1,81 @@
 import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import PostsFeed from '@/widgets/PostsFeed';
 import CreatePostModal from '@/features/CreatePostModal';
 import { useAuth } from '@/entities/auth/useAuth';
 import Button from '@/shared/ui/Button';
-import { SUGGESTED_COMMUNITIES_CARDS_DATA, SUGGESTED_USERS_CARDS_DATA } from '@/shared/mocks/CardsListMocks';
+import { getGroups } from '@/entities/Group/api/getGroups';
+import { getSuggestedUsers } from '@/entities/User/api/getSuggestedUsers';
+import { getUserDisplayName } from '@/entities/User/utilities';
+import type { GroupModel } from '@/entities/Group/types';
+import type { SuggestedUserModel } from '@/entities/User/types';
 import CardsList from './CardsList';
+import type { CardData } from './CardsList';
 import './style.css';
 
-const MAX_POST_FILE_SIZE  = 10 * 1024 * 1024;
+const MAX_POST_FILE_SIZE = 10 * 1024 * 1024;
 const ACCEPTED_POST_FILE_TYPES = ['image/png', 'image/jpeg'];
+const MAX_SUGGESTED_USERS_COUNT = 5;
+const MAX_SUGGESTED_GROUPS_COUNT = 3;
+
+const numberFormatter = new Intl.NumberFormat('en-GB', {
+  notation: 'compact',
+  compactDisplay: 'short',
+  maximumFractionDigits: 1,
+});
+
+function suggestedUserToCardDataView(user: SuggestedUserModel): CardData {
+  return {
+    id: user.id,
+    pictureUrl: user.photo,
+    title: getUserDisplayName(user),
+    subtitle: user.username.startsWith('@') ? user.username : `@${user.username}`,
+  };
+}
+
+function groupToCardDataView(group: GroupModel): CardData {
+  let membersCountView;
+
+  if (group.membersCount === 1) {
+    membersCountView = '1 member';
+  } else {
+    membersCountView = `${numberFormatter.format(group.membersCount).toLowerCase()} members`;
+  }
+
+  return {
+    id: group.id,
+    pictureUrl: group.photo,
+    title: group.title,
+    subtitle: membersCountView,
+  }
+}
 
 function HomePage() {
   const [isCreatePostModalOpen, setIsCreatePostModalOpen] = useState(false);
   const [quickPostContent, setQuickPostContent] = useState('');
   const { currentUser } = useAuth();
+
+  const {
+    data: suggestedUsers,
+    isError: isSuggestedUsersQueryError,
+    isPending: isSuggestedUsersQueryPending,
+  } = useQuery({
+    queryKey: ['suggestedUsers'],
+    queryFn: ({ signal }) => getSuggestedUsers(signal),
+    select: (users) => users.slice(0, MAX_SUGGESTED_USERS_COUNT).map(suggestedUserToCardDataView),
+    enabled: currentUser !== null,
+  });
+
+  const {
+    data: suggestedGroups,
+    isError: isGroupsQueryError,
+    isPending: isGroupsQueryPending,
+  } = useQuery({
+    queryKey: ['groups'],
+    queryFn: ({ signal }) => getGroups(signal),
+    select: (groups) => groups.slice(0, MAX_SUGGESTED_GROUPS_COUNT).map(groupToCardDataView),
+    enabled: currentUser !== null,
+  });
 
   function handleCreatePostModalOpen() {
     setIsCreatePostModalOpen(true);
@@ -74,17 +136,27 @@ function HomePage() {
         <PostsFeed />
       </section>
 
-      <aside className='home-page-suggested'>
-        <h2 className='visually-hidden'>Suggestions</h2>
-        <CardsList
-          title='Suggested people'
-          cardsData={SUGGESTED_USERS_CARDS_DATA}
-        />
-        <CardsList
-          title='Communities you might like'
-          cardsData={SUGGESTED_COMMUNITIES_CARDS_DATA}
-        />
-      </aside>
+      {currentUser &&
+        <aside className='home-page-suggested'>
+          <h2 className='visually-hidden'>Suggestions</h2>
+          <CardsList
+            title='Suggested people'
+            cardsData={suggestedUsers}
+            isDataFetchPending={isSuggestedUsersQueryPending}
+            isDataFetchError={isSuggestedUsersQueryError}
+            errorMessage='Cannot load suggested people'
+            emptyListMessage='No suggested people'
+          />
+          <CardsList
+            title='Communities you might like'
+            cardsData={suggestedGroups}
+            isDataFetchPending={isGroupsQueryPending}
+            isDataFetchError={isGroupsQueryError}
+            errorMessage='Cannot load suggested groups'
+            emptyListMessage='No suggested groups'
+          />
+        </aside>
+      }
     </div>
 
   );
