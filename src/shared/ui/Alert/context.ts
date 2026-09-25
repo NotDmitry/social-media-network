67df0d6efed20feb0dcefdab40881f59f4767931
Dzmitry Buldyha <91372817+NotDmitry@users.noreply.github.com>
2026-09-25 16:40:19 +0300

    refactor: remove single active alert responsibilities

--- src/shared/ui/Alert/context.ts
+++ src/shared/ui/Alert/context.ts
@@ -3,7 +3,6 @@ import type { AlertSeverityLevel } from './index';
 
 interface AlertContextData {
   showAlert: (message: string, severity?: AlertSeverityLevel) => void;
-  closeAlert: () => void;
 }
 
 export const AlertContext = createContext<AlertContextData | null>(null);
--- src/shared/ui/Alert/index.tsx
+++ src/shared/ui/Alert/index.tsx
@@ -1,29 +1,46 @@
-import { createPortal } from 'react-dom';
+import { useCallback, useEffect } from 'react';
 import { CrossIcon } from '@/shared/icons';
 import './style.css';
 
 export type AlertSeverityLevel = 'success' | 'warning' | 'error';
 
 export interface AlertProps {
+  id: string;
   message: string;
   severity?: AlertSeverityLevel;
-  onClose?: () => void;
+  duration?: number;
+  onClose?: (alertId: string) => void;
 }
 
-function Alert({ message, severity = 'success', onClose }: AlertProps) {
-  return createPortal(
+function Alert({ id, message, severity = 'success', duration, onClose }: AlertProps) {
+  const handleAlertClose = useCallback(() => {
+    onClose?.(id);
+  }, [id, onClose]);
+
+  useEffect(() => {
+    if (duration === undefined) {
+      return;
+    }
+
+    const alertTimerId = setTimeout(handleAlertClose, duration);
+
+    return () => {
+      clearTimeout(alertTimerId);
+    };
+  }, [duration, handleAlertClose]);
+
+  return (
     <div className={`alert alert_${severity}`}>
       <p className='alert-message'>{message}</p>
       <button
         className='alert-button'
         type='button'
-        onClick={onClose}
+        onClick={handleAlertClose}
         aria-label='Close notification'
       >
         <CrossIcon />
       </button>
-    </div>,
-    document.body
+    </div>
   );
 }
 
